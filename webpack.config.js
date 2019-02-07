d0c3baa27d6ed3aedd8a5a5ddb08acc400bfa52c
Mina Sorsok <mina.sorsok@gmail.com>
2019-02-06 19:57:58 -0800

    Install more dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, './client/src'),
+  entry: path.resolve(__dirname, './client/'),
   output: {
-    path: path.resolve(__dirname, './client/dist'),
+    path: path.resolve(__dirname, './public'),
     filename: 'bundle.js'
   },
   module: {
@@ -14,7 +14,12 @@ module.exports = {
         options: {
           presets: ['@babel/preset-env', '@babel/preset-react']
         }
+      },
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
-  }
+  },
+  resolve: { extensions: ['.js', '.jsx'] }
 };
