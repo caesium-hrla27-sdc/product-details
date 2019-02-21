32e8471e4abc979215522891c97295abbbe821a2
Mina Sorsok <mina.sorsok@gmail.com>
2019-02-20 16:24:36 -0800

    Move flashbox into own component

--- client/components/App/ProductDetails/ProductSummary/ProductShopping/ProductShopping.jsx
+++ client/components/App/ProductDetails/ProductSummary/ProductShopping/ProductShopping.jsx
@@ -1,109 +1,21 @@
 import React from 'react';
 
 import styles from './style.css';
-import { SSL_OP_CRYPTOPRO_TLSEXT_BUG } from 'constants';
+
+import FlashBox from './FlashBox/FlashBox';
 
 class ProductShopping extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      fillCheckbox: false
-    };
-    this.toggleCheckbox = this.toggleCheckbox.bind(this);
-  }
-
-  toggleCheckbox(event) {
-    this.setState({
-      fillCheckbox: this.state.fillCheckbox ? false : true
-    });
   }
 
   render() {
     return (
       <div id={styles.container}>
-        <div id={styles.flashBox}>
-          <label id={styles.flashLabel}>
-            <input
-              type="checkbox"
-              id={styles.flashInput}
-              value=""
-              onClick={this.toggleCheckbox}
-            />
-
-            <div
-              id={styles.checkboxFilled}
-              style={
-                this.state.fillCheckbox
-                  ? { backgroundColor: 'rgb(0, 0, 0)' }
-                  : {}
-              }
-            >
-              <svg
-                viewBox="0 0 32 32"
-                id={styles.svgCheckbox}
-                style={this.state.fillCheckbox ? {} : { display: 'none' }}
-              >
-                <path d="M1 14 L5 10 L13 18 L27 4 L31 8 L13 26 z" />
-              </svg>
-            </div>
-
-            <div id={styles.checkboxLabelContainer1}>
-              <div>
-                <div>
-                  <div>
-                    <button id={styles.checkboxLabelButton} type="button">
-                      <p>
-                        Yes, I want <strong>FREE 2-Day Shipping</strong> with
-                        Sephora FLASH. <ins>Learn More</ins>
-                      </p>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </label>
-        </div>
+        <FlashBox />
 
         <div id={styles.shoppingBottom}>
-          <div id={styles.bottomLeftContainer}>
-            <label id={styles.quantityLabel}>Quantity</label>
-            <div id={styles.dropdownContainer1}>
-              <div id={styles.dropdownContainer2}>
-                <select id={styles.dropdown}>
-                  <option value="1" className={styles.option}>
-                    1
-                  </option>
-                  <option value="2" className={styles.option}>
-                    2
-                  </option>
-                  <option value="3" className={styles.option}>
-                    3
-                  </option>
-                  <option value="4" className={styles.option}>
-                    4
-                  </option>
-                  <option value="5" className={styles.option}>
-                    5
-                  </option>
-                  <option value="6" className={styles.option}>
-                    6
-                  </option>
-                  <option value="7" className={styles.option}>
-                    7
-                  </option>
-                  <option value="8" className={styles.option}>
-                    8
-                  </option>
-                  <option value="9" className={styles.option}>
-                    9
-                  </option>
-                  <option value="10" className={styles.option}>
-                    10
-                  </option>
-                </select>
-              </div>
-            </div>
-          </div>
+          <Dropdown />
           <div id={styles.bottomRightContainer}>
             <div id={styles.basketLoves}>
               <div>
