47f999e7746eba3ef703fcb3676125db7549a3ad
Bryant Lin <br4qua@gmail.com>
2019-03-07 10:57:39 -0800

    BUGFIX: Corrected patch request behavior

--- server/index.js
+++ server/index.js
@@ -4,10 +4,8 @@ const morgan = require('morgan');
 const parser = require('body-parser');
 const path = require('path');
 
-const {	Product } = require('./databases/index_mongo');
-const
-	pool = require('./databases/index_postgres');
-
+// const {	Product } = require('./databases/index_mongo');
+const pool = require('./databases/index_postgres');
 const PORT = process.env.PORT || 3002;
 const app = express();
 
@@ -17,47 +15,107 @@ app.use(parser.json());
 app.use(parser.urlencoded({	extended: true}));
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-const useMongo = false;
-const usePG = !useMongo;
+var maxRange = 9999999;
 
 app.get('/productDetails/', (req, res) => {
 
-	var tempid = Math.floor(Math.random() * 1000);
-
-	if (useMongo) {
-		Product.findOne({
-			id: tempid
-		}).then(product => {
-			console.log("hey", product)
-			res.status(200).json(product);
-		});
-	}
-
-	if (usePG) {
-		pool.query('SELECT * FROM products WHERE id =' + tempid, (err, result) => {
-			console.log(...result.rows) //deconstructing
-			// pool.end()
+		var tempID = Math.floor(Math.random() * maxRange );
+		pool.query('SELECT * FROM products WHERE id =' + tempID, (err, result) => {
 			res.status(200).json(result.rows[0]);
 		})
-	}
 
 });
 
 app.get('/productDetails/:id', (req, res) => {
-	if (useMongo) {
 
-		Product.findOne({
-			id: req.params.id
-		}).then(product => {
-			res.status(200).json(product);
-		});
-	}
-
-	if (usePG) {
 		pool.query('SELECT * FROM products WHERE id =' + req.params.id, (err, result) => {
+			if (err) { res.status(404).send('db err', err);}
 			res.status(200).json(result.rows[0]);
 		})
+
+});
+
+//CRUD EXTENSION
+
+app.post('/productDetails/:id', (req, res) => {
+
+		var numb = +1e7*2+req.params.id;
+		var qstring = "INSERT INTO products (id) VALUES ("+numb+")";
+
+		pool.query(qstring, (err, result) => {
+			if (err) { 
+				console.log("ERROR", err)
+				res.status(404).send('db err');
+			} else if (result){
+				res.status(200).send('successful insert');
+			}
+		})
+		
+});
+
+
+
+
+
+app.put('/productDetails/:id', (req, res) => {
+	
+	var testString = "Updated " + new Date().toUTCString() ;
+	var qstring= `UPDATE products SET description = '${testString}' WHERE id = `+req.params.id;
+	
+	pool.query(qstring, (err, result) => {
+		if (err) {
+			console.log("ERROR", err)
+			res.status(404).send('db err');
+		} else if (result) {
+			if (result.rowCount === 0) {
+				res.status(404).send('no matching entry');
+			} else {
+				res.status(200).send('successful delete');
+			}
+		}
+	})
+
+
+});
+
+app.patch('/productDetails/:id', (req, res) => {
+	
+	var testString = "Updated " + new Date().toUTCString() ;
+	var qstring= `UPDATE products SET description = '${testString}' WHERE id = `+req.params.id;
+	
+	pool.query(qstring, (err, result) => {
+		if (err) {
+			console.log("ERROR", err)
+			res.status(404).send('db err');
+		} else if (result) {
+			if (result.rowCount === 0) {
+				res.status(404).send('no matching entry');
+			} else {
+				res.status(200).send('successful delete');
+			}
+		}
+	})
+
+
+});
+
+app.delete('/productDetails/:id', (req, res) => {
+	
+	var qstring= "DELETE FROM products WHERE id = "+req.params.id;
+
+	pool.query(qstring, (err, result) => {
+	if (err) {
+		console.log("ERROR", err)
+		res.status(404).send('db err');
+	} else if (result) {
+		if (result.rowCount === 0) {
+			res.status(404).send('no such entry');
+		} else {
+			res.status(200).send('successful delete');
+		}
 	}
+})
+
 });
 
 app.listen(PORT, () => console.log(`server is listening on PORT: ${PORT}`));
